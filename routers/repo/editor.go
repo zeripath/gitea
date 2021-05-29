08d5f09cec8d843e10639f504dd4afc46b4cd66d
Andrew Thornton <art27@cantab.net>
2021-05-29 19:41:29 +0100

    Close the dataRC reader sooner (#16023)
    Fix #15932
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- routers/repo/editor.go
+++ routers/repo/editor.go
@@ -105,6 +105,7 @@ func editFile(ctx *context.Context, isNewFile bool) {
 			ctx.NotFound("blob.Data", err)
 			return
 		}
+
 		defer dataRc.Close()
 
 		ctx.Data["FileSize"] = blob.Size()
@@ -121,6 +122,10 @@ func editFile(ctx *context.Context, isNewFile bool) {
 		}
 
 		d, _ := ioutil.ReadAll(dataRc)
+		if err := dataRc.Close(); err != nil {
+			log.Error("Error whilst closing blob data: %v", err)
+		}
+
 		buf = append(buf, d...)
 		if content, err := charset.ToUTF8WithErr(buf); err != nil {
 			log.Error("ToUTF8WithErr: %v", err)
